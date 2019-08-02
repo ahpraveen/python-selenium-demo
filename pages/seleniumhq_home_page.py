12db59e58a5cafcf0ff5a8ba9689ed7a3e376884
ahpraveen <aha.praveen@gmail.com>
2019-08-02 17:19:41 -0400

    Handling error

--- pages/seleniumhq_home_page.py
+++ pages/seleniumhq_home_page.py
@@ -1,14 +1,19 @@
 from selenium import webdriver
 
 def launch_browser(browser_name):
-    if browser_name == "chrome":
-        mydriver = webdriver.Chrome("../../driver/chromedriver.exe")
-    elif browser_name == "firefox":
-        mydriver = webdriver.Firefox()
-    else:
-        mydriver = webdriver.Ie('../../driver/IEDriverServer.exe')
-    mydriver.get("https://www.seleniumhq.org/")
-    return mydriver
+    try:
+        if browser_name == "chrome":
+            mydriver = webdriver.Chrome("../../driver/chromedriver.exe")
+        elif browser_name == "firefox":
+            mydriver = webdriver.Firefox()
+        elif browser_name == "ie":
+            mydriver = webdriver.Ie('../../driver/IEDriverServer.exe')
+        else:
+            raise NameError('Error')
+        mydriver.get("https://www.seleniumhq.org/")
+        return mydriver
+    except NameError:
+        return "browser name is incorrect"
 
 def click_about_link(driver):
     driver.find_element_by_partial_link_text("About").click()
