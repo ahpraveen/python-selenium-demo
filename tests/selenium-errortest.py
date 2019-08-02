927c9fead7d87249bf24c0f07f812b5749aa5b32
ahpraveen <aha.praveen@gmail.com>
2019-08-02 17:23:49 -0400

    Updated actual and expected

--- tests/selenium-errortest.py
+++ tests/selenium-errortest.py
@@ -5,4 +5,4 @@ class SeleniumTest(unittest.TestCase):
 
     def test_invalid_browser_name(self):
         self.driver = seleniumhq_home_page.launch_browser("invalidbrowser")
-        self.assertEqual("browser name is incorrect", "browser name is incorrect")
+        self.assertEqual("browser name is incorrect", self.driver)
