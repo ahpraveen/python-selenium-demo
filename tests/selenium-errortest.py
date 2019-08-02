cb0942a0d7be97275c11f856fee88d5c5ca73b14
ahpraveen <aha.praveen@gmail.com>
2019-08-02 17:21:36 -0400

    Updated the data - invalid browser name.

--- tests/selenium-errortest.py
+++ tests/selenium-errortest.py
@@ -4,5 +4,5 @@ from pages import seleniumhq_home_page
 class SeleniumTest(unittest.TestCase):
 
     def test_invalid_browser_name(self):
-        self.driver = seleniumhq_home_page.launch_browser("not-ie-chrome-firefox")
+        self.driver = seleniumhq_home_page.launch_browser("invalidbrowser")
         self.assertEqual("browser name is incorrect", "browser name is incorrect")
